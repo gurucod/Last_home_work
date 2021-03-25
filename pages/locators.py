8c70f9a1023fa3b72198c513585964fc613e4b86
Faiz <faiz1992@mail.ru>
2021-03-25 15:17:25 +0300

    Добавлен переход между страницами "Главная" и "Авторизация/Регистрация"

--- pages/locators.py
+++ pages/locators.py
@@ -9,9 +9,3 @@ class LoginPageLocators():
     FORM_LOGIN_ID = (By.CSS_SELECTOR, "#login_form")
     FORM_REGISTER_ID = (By.CSS_SELECTOR, "#register_form")
 
-    # REGISTR_MAIL_LINK = (By.CSS_SELECTOR, "#id_registration-email")
-    # REGISTR_PASS_LINK = (By.CSS_SELECTOR, "id_registration-password1")
-    # REGISTR_PASS_CONF = (By.CSS_SELECTOR, "id_registration-password2")
-    #
-    # AUTH_MAIL_LINK = (By.CSS_SELECTOR, "id_login-username")
-    # AUTH_PASS_LINK = (By.CSS_SELECTOR, "id_login-password")
