b947a0ab4efb205b732b18104d71aa4496cb7b0e
Faiz <faiz1992@mail.ru>
2021-03-25 19:52:53 +0300

    Добавлены описания основных методов классов

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 
 class BasePage():
+    """Базовый класс который поключает браузер и url. Так же добавляет методы open и
+    is_element_present для перехода на страницу по url и ловит ошибки NoSuchElementException"""
 
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -12,8 +16,26 @@ class BasePage():
         self.browser.get(self.url)
 
     def is_element_present(self, how, what):
+        """Метод BasePage. Метод is_element_present, в котором перехватываются исключения.
+        В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать
+        (строку-селектор)."""
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        """Метод в тесте для получения проверочного кода"""
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,10 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 #import time
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
