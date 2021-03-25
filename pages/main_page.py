fbf541b34c29f344bbb19e3c6f533ae2ebfe5dd5
Faiz <faiz1992@mail.ru>
2021-03-25 15:27:45 +0300

    Добавлен переход между страницами "Главная" и "Авторизация/Регистрация"

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,6 +9,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
         #time.sleep(10)
 
     def should_be_login_link(self):
