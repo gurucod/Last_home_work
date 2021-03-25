7adf00fecc4074d3e519a9d56c88219a6f9a23bf
Faiz <faiz1992@mail.ru>
2021-03-25 19:51:21 +0300

    Обновлен файл локаторов. Добавлены локаторы страницы "карточка товара"

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators():
     FORM_LOGIN_ID = (By.CSS_SELECTOR, "#login_form")
     FORM_REGISTER_ID = (By.CSS_SELECTOR, "#register_form")
 
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRICE_BOOK_IN_PAGE = (By.CSS_SELECTOR, "div.product_main p.price_color")
+    PRICE_BOOK_IN_BASKET = (By.CSS_SELECTOR, "div#messages div:nth-child(3) div.alertinner strong")
+    NAME_BOOK_IN_PAGE = (By.CSS_SELECTOR, "div.product_main h1")
+    NAME_BOOK_IN_BASKET = (By.CSS_SELECTOR, "div#messages div:nth-child(1) strong")
+    MASSAGE_SUCCESSFUL_ADD = (By.CSS_SELECTOR, "div#messages div:nth-child(1) div.alertinner")
