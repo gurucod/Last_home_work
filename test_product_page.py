e4a318ea33a62ef551cbec1a618c164d7e48cfde
Faiz <faiz1992@mail.ru>
2021-03-26 10:26:57 +0300

    Добавлена mark.parametrize с перебором ссылок.

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,15 @@
-from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
+import pytest
 import time
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize('promo_code', [pytest.param(i, marks=pytest.mark.xfail(i==7, reason='Забагованный тест'))
+                                        for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_code):
+    url = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_code}"
+    page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_product_page()
-    time.sleep(2)
+    #time.sleep(2)
