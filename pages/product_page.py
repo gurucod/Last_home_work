5769005c5e3e0494f3ebadbd686c66ac90a8f2ac
Faiz <faiz1992@mail.ru>
2021-03-26 10:28:06 +0300

    Обновлена проверка на URL (более унифицирована). Закоментил не нужный блок кода

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,23 +7,23 @@ class ProductPage(BasePage):
         """Тут вызываются методы проверки пристутсвия элементов на странице"""
         self.should_be_login_url()
         self.should_be_button_basket()
-        self.should_be_book_in_basket()
+        #self.should_be_book_in_basket()
         self.should_be_book_name_equael()
         self.should_be_price_basket_equale_book_price()
 
     def should_be_login_url(self):
         """Проверка что мы находимся на нужной странице"""
-        assert "?promo=newYear" in self.url, "Переход неверную карточку товара"
+        assert "/catalogue/coders-at-work_207/" in self.url, "Переход неверную карточку товара"
 
     def should_be_button_basket(self):
         """Проверка что кнопка добавления в корзину найдена"""
         assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_BASKET), "Кнопка добавления в " \
                                                                                    "корзину не найдена на странице"
 
-    def should_be_book_in_basket(self):
-        """Проверка наличия сообщения об успешном добавлении товара"""
-        message = self.browser.find_element(*ProductPageLocators.MASSAGE_SUCCESSFUL_ADD).text
-        assert "был добавлен в вашу корзину" in message, "Сообщение об успешном добавлении не совпало"
+    # def should_be_book_in_basket(self):
+    #     """Проверка наличия сообщения об успешном добавлении товара"""
+    #     message = self.browser.find_element(*ProductPageLocators.MASSAGE_SUCCESSFUL_ADD).text
+    #     assert "был добавлен в вашу корзину" in message, "Сообщение об успешном добавлении не совпало"
 
     def should_be_price_basket_equale_book_price(self):
         """Проверяется что стоимость корзины совпадает с ценой товара."""
