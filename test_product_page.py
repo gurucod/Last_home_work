f882b222966b9d591591d37599738d6b4177e3a6
Faiz <faiz1992@mail.ru>
2021-03-29 17:15:52 +0300

    Раскоментил параметризованный тест и пометил его как Skip

--- test_product_page.py
+++ test_product_page.py
@@ -87,13 +87,13 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_be_message_basket_empty()
 
 
-# @pytest.mark.skip
-# @pytest.mark.parametrize('promo_code', [pytest.param(i, marks=pytest.mark.xfail(i==7, reason='Забагованный тест'))
-#                                         for i in range(10)])
-# def test_guest_can_add_product_to_basket(browser, promo_code):
-#     url = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_code}"
-#     page = ProductPage(browser, url)
-#     page.open()
-#     page.should_be_product_page()
-#     page.add_to_basket()
-#     page.solve_quiz_and_get_code()
+@pytest.mark.skip
+@pytest.mark.parametrize('promo_code', [pytest.param(i, marks=pytest.mark.xfail(i==7, reason='Забагованный тест'))
+                                        for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_code):
+    url = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_code}"
+    page = ProductPage(browser, url)
+    page.open()
+    page.should_be_product_page()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
