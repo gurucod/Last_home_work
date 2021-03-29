f8d6868e07741e8f5183a025c74088a56ff9e366
Faiz <faiz1992@mail.ru>
2021-03-29 15:10:35 +0300

    Добавлен маркер @pytest.mark.login_guest

--- pages/base_page.py
+++ pages/base_page.py
@@ -24,7 +24,7 @@ class BasePage():
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
-    def should_be_login_page(self):
+    def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def go_to_basket_page(self):
